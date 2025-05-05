71428da191842b013c0d737311e105def7e5ffa3
ryan-gabriel <ryangs112233@gmail.com>
2025-05-05 11:15:39 +0700

    6.15 Anecdotes and the Backend, step 2

--- redux-anecdotes/src/components/AnecdoteForm.jsx
+++ redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,15 +1,16 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { showNotification } from "../reducers/notificationReducer";
-
+import anecdoteService from "../services/anecdotes";
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const anecdote = event.target.anecdote.value;
     event.target.anecdote.value = "";
-    dispatch(createAnecdote(anecdote));
-    dispatch(showNotification(`you created '${anecdote}'`, 5));
+    const newAnecdote = await anecdoteService.createNew(anecdote);
+    dispatch(createAnecdote(newAnecdote));
+    dispatch(showNotification(`you created '${anecdote}'`));
   };
   return (
     <>
--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,15 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const getId = () => (100000 * Math.random()).toFixed(0);
-
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
-
 const anecdoteSlice = createSlice({
   name: "anecdotes",
   initialState: [],
@@ -26,8 +16,7 @@ const anecdoteSlice = createSlice({
       );
     },
     createAnecdote(state, action) {
-      const newAnecdote = asObject(action.payload);
-      state.push(newAnecdote);
+      state.push(action.payload);
     },
     appendAnecdote(state, action) {
       state.push(action.payload);
